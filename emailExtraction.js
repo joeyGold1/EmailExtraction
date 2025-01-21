66677e0a36038b13d8e60f395f8563d1effea5fa
Joey Gold <joey.gold@softwire.com>
2025-01-21 14:05:55 +0000

    Refactor into function

--- emailExtraction.js
+++ emailExtraction.js
@@ -3,12 +3,16 @@ const getFileContents = () => {
     return fs.readFileSync('test.txt', 'utf8');
 }
 
-const data = getFileContents();
-let counter = 0;
-for (let index = 0; index < data.length; index++) {
-    if (data.substring(index, index + 13) === '@softwire.com') {
-        counter++;
+const naiveCounter = (data) => { 
+    let counter = 0;
+    for (let index = 0; index < data.length; index++) {
+        if (data.substring(index, index + 13) === '@softwire.com') {
+            counter++;
+        }
     }
+    return counter
 }
 
-console.log(counter);
+const data = getFileContents();
+const count = naiveCounter(data);
+console.log(count);
