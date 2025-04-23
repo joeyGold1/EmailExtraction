a440285af37e40826c370a7abec37bb62a672038
Joey Gold <joey.gold@softwire.com>
2025-04-23 17:02:40 +0100

    Clean up

--- emailExtraction.js
+++ emailExtraction.js
@@ -3,40 +3,40 @@ const getFileContents = () => {
     return fs.readFileSync('test.txt', 'utf8');
 }
 
-const naiveCounter = (data) => { 
+const naiveSoftwireAddressCounter = (data) => { 
     let counter = 0;
     for (let index = 0; index < data.length; index++) {
         if (data.substring(index, index + 13) === '@softwire.com') {
             counter++;
         }
     }
-    return counter
+    return counter;
 }
 
-const regexCounter = (data) => {
-    const re = /\b[\w\.]{3}\w*@softwire.com\b/g;
-    const matches = [...data.matchAll(re)];
+const regexSoftwireAddressCounter = (data) => {
+    const softwireEmailAddressRegex = /\b[\w\.]{3}\w*@softwire.com\b/g;
+    const matches = [...data.matchAll(softwireEmailAddressRegex)];
     return matches.length;
 }
 
 const domainCounter = (data) => {
-    const re = /\w[\w\.]+\w@((\w+\.\w+)+)(?=\s)/g;
-    const matches = [...data.matchAll(re)];
-    const domains = {};
+    const arbitraryEmailAddressRegex = /\w[\w\.]+\w@((\w+\.\w+)+)(?=\s)/g;
+    const matches = [...data.matchAll(arbitraryEmailAddressRegex)];
+    const domainCounts = {};
     for (const match of matches) {
         const domain = match[1];
-        if (domains[domain] !== undefined) {
-            domains[domain] += 1;
+        if (domainCounts[domain] !== undefined) {
+            domainCounts[domain] += 1;
         } else {
-            domains[domain] = 1;
+            domainCounts[domain] = 1;
         }
     }
-    return domains;
+    return domainCounts;
 }
 
 const data = getFileContents();
-const naiveCount = naiveCounter(data);
-const regexCount = regexCounter(data);
+const naiveCount = naiveSoftwireAddressCounter(data);
+const regexCount = regexSoftwireAddressCounter(data);
 const domains = domainCounter(data);
 console.log(`Naive count of softwire.com: ${naiveCount}`);
 console.log(`Regex count of softwire.com: ${regexCount}`);
